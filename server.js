911652fcb54c6155b08a79663353b77c4698dfd8
tenieceb <tabusenbark@gmail.com>
2026-05-18 14:28:35 -0600

    updated server.js to include middleware to make node_env available and updated footer to show if it's in development mode

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.use((req, res, next) => {
     next(); // Pass control to the next middleware or route
 });
 
+// Middleware to make NODE_ENV available to all templates
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
 
 /**
   * Routes
