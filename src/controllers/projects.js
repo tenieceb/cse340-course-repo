429fb01a699312415a78a96006b96b0f5f5d4525
tenieceb <tabusenbark@gmail.com>
2026-05-18 15:31:13 -0600

    restructuring to the MVC pattern

---
+++ src/controllers/projects.js
@@ -0,0 +1,9 @@
+import { getAllProjects } from '../models/projects.js';
+
+const projectsPage = async (req, res) => {
+    const projects = await getAllProjects();
+    const title = 'Service Projects';
+
+    res.render('projects', { title, projects });
+};
+export { projectsPage };
