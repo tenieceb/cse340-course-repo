7ea5e32cd6194b5a9bd118195954aa1f6daef912
tenieceb <tabusenbark@gmail.com>
2026-06-01 16:03:00 -0600

    Learning Activities w04 1-4

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
 import router from './src/routes.js';
+import session from 'express-session';
+import flash from './src/middleware/flash.js'
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -18,6 +21,21 @@ const app = express();
 /**
   * Configure Express middleware
   */
+
+// Set up session management
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60 * 60 * 1000 } // Session expires after 1 hour of inactivity
+}));
+
+// Use flash message middleware
+app.use(flash);
+
+// Allow Express to receive and process common POST data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 
 import { homePage } from './controllers/index.js';
-import { organizationsPage, showOrganizationDetailsPage } from './controllers/organizations.js';
+import { organizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm, organizationValidation, showEditOrganizationForm, processEditOrganizationForm
+} from './controllers/organizations.js';
 import { projectsPage, showProjectDetailsPage } from './controllers/projects.js';
 import { categoriesPage,showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage } from './controllers/errors.js';
@@ -15,7 +16,10 @@ router.get('/projects', projectsPage);
 router.get('/project/:id', showProjectDetailsPage);
 router.get('/categories', categoriesPage);
 router.get('/category/:id', showCategoryDetailsPage);
-
+router.get('/new-organization', showNewOrganizationForm);
+router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+router.get('/edit-organization/:id', showEditOrganizationForm);
+router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
 // error-handling routes
 router.get('/test-error', testErrorPage);
 
